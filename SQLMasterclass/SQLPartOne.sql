4aa9709e3d5c8917715418edb26fe84790f1285b
Tomas Venere <tomasvenere@Tomass-Laptop.local>
2024-06-25 17:54:12 -0400

    40% done

--- SQLMasterclass/SQLPartOne.sql
+++ SQLMasterclass/SQLPartOne.sql
@@ -105,3 +105,85 @@ FROM shippers
 UNION
 SELECT first_name
 FROM customers
+
+--INSERT
+INSERT INTO customers
+VALUES 
+    (DEFAULT, 
+    'Jhon', 
+    'Smith',
+    '1990-01-01',
+    Null,
+    'address',
+    'city',
+    'CA',
+    DEFAULT)
+
+--Create Archived table
+CREATE TABLE orders_archived
+SELECT * FROM orders
+
+--Copy Table for archive 
+INSERT INTO orders_archived
+SELECT *
+From orders
+Where order_date < '2019-01-01'
+
+--UPDATE invoices
+UPDATE invoices
+SET payment_total = 10, payment_date = '2019-03-01'
+WHERE invoice id = 1
+
+--UPDATE multiple roads invoices
+UPDATE invoices
+SET payment_total = 10, payment_date = '2019-03-01'
+WHERE client_id IN (3,4)
+
+--SUBQUERIES 
+UPDATE invoices
+SET payment_total = 10, payment_date = '2019-03-01'
+WHERE invoice id = (SELECT client_id 
+                    FROM clients 
+                    WHERE state IN ('CA', 'NY'))
+
+--SUBQUERIES NOT IN
+UPDATE invoices
+SET payment_total = 10, payment_date = '2019-03-01'
+WHERE invoice id NOT IN (SELECT client_id 
+                    FROM clients 
+                    WHERE state IN ('CA', 'NY'))
+
+--DELETE 
+DELETE FROM invoices
+WHERE invoice_id = 1
+
+--Aggregate Functions (MAX, MIN, AVG, SUM, COUNT)
+SELECT 
+    MAX(invoice_total) AS highest
+FROM invoices
+
+--GROUP BY
+SELECT client_id, SUM(invoice_total) AS total_sales
+FROM invoices
+GROUP BY client_id
+
+--HAVING: you can use HAVING as a WHERE after a GROUP BY
+
+--GROUP BY with ROLLUP
+SELECT 
+	client_id, 
+    SUM(invoice_total) AS total_sales
+FROM invoices
+GROUP BY client_id WITH ROLLUP
+
+--Common Functions: ROUND(), CEILING(), TRUNCATE(), FLOOR(), ABS(), RAND()
+--String Functions: LENGTH('example'),...
+--Date Functions: NOW(), CURDATE(),...
+--Formating Dates F: DATE_FORMAT(NOW(), '%M %d %Y')
+--Calculating Dates and Time DATE_ADD(NOW(), INTERVAL 1 DAY)
+--IFNUL and COALESCE Func: IFNULL(shiper_id, 'Not Assigned')
+--IF Func: IF(expression, first, second)
+--CASE Operator: CASE WHEN ... WHEN ...
+-- = ANY is equal to IN operatior
+-- EXIST is more efficient than IN
+-- You can use sub queries for SELECT and FROM
